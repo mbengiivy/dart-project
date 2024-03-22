b964e68f1db88a093b6ad04d31c8279510f991b2
Ivy <mbengiivy@gmail.com>
2024-03-22 08:48:58 +0300

    Week 2 assignment

---
+++ main.dart
@@ -0,0 +1,34 @@
+//THIS IS A FILE FOR ASSIGNMENT WEEK 2
+
+void main() {
+  //dart print
+  print('Hello world.');
+
+  //dart variables-lesson one
+  String name = "John"; //String is for non-numbers
+  num umri = 30; //num is for both int and double
+  int birthdate = 25;
+  double subjects = 25.0;
+  print("My name is $name,I am  $umri years old, born in $birthdate and I do $subjects subjects");
+
+  //DECLARING VARS AND DATA TYPES
+  //NUM-INT AND FLOAT/DOUBLE
+  num num1 = 40;
+  print("Num 1 is $num1");
+
+  //LIST
+  List<String> someNames = ["Jack", "John", "Ian"];
+  print("The name in position 0 is ${someNames[0]}");
+
+  //MAPS
+  Map<String, String> animalSounds = {"Cow": "Moo", "Dog": "Bark"};
+  print("The nearest animal sound is $animalSounds");
+
+  
+}
+
+
+//The program should demonstrate an understanding of data types in Dart, including int, double, String, List, and Map.
+//The program should include at least one example of each data type.
+//The program should include comments to explain each data type and its use.
+//The program should be tested for accuracy and performance.
